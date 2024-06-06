8340af0890c5f17a6f0c2f9468788ff0a5b6edc0
tharoyng <tharo.yng@gmail.com>
2024-06-06 14:47:25 +0700

    show/hide faq answer

--- education website/main.js
+++ education website/main.js
@@ -4,3 +4,20 @@ window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle
     ('window-scroll', window.scrollY > 100)
 })
+
+//show/hide faq answer
+
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+    //changeicon
+    const icon = faq.querySelector('.faq__icon i');
+    if (icon.className === 'uil uil-angle-up') {
+        icon.className = "uil uil-angle-down"
+    } else {
+        icon.className = "uil uil-angle-up"
+    }
+    })
+})
